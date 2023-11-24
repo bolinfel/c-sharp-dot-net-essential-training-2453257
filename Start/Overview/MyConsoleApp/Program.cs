cd2c95da1199c33f31d037f2e211015ae2fc3ee5
bolinfel1 <bolifel@gmail.com>
2023-11-24 11:46:24 -0300

    Completed Overview of .net chapter

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+//Declare global Variables
+string response;
+OperatingSystem thisOs = default;
+
+//Asks for user Name and prints a welcome message
+Console.WriteLine("What is your name? ");
+response  = Console.ReadLine();
+Console.WriteLine($"Welcome {response}");
+
+//print out user OS version
+thisOs = Environment.OSVersion;
+Console.WriteLine(thisOs.Platform);
+Console.WriteLine(thisOs.VersionString);
